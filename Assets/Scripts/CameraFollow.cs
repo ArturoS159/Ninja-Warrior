64e4691e208afd5cade7157cac384d84e91e0f15
ArturoS159 <akowalski3677@gmail.com>
2019-11-16 18:02:39 +0100

    move, attack anim

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,15 +1,13 @@
-﻿using UnityEngine;
+using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
-{
+public class CameraFollow : MonoBehaviour {
     public float FollowSpeed = 4f;
     public Transform Target;
-    private void Update()
-    {
-        if(Target!=null){
-        Vector3 newPosition = Target.position;
-        newPosition.z = -10;
-        transform.position = Vector3.Slerp(transform.position, newPosition, FollowSpeed * Time.deltaTime);
+    private void Update () {
+        if (Target != null) {
+            Vector3 newPosition = Target.position;
+            newPosition.z = -10;
+            transform.position = Vector3.Slerp (transform.position, newPosition, FollowSpeed * Time.deltaTime);
         }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,82 +1,92 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     // public int hp=5;
-    // public float sprintSpeed = 4f;
-    public float walkSpeed = 7f;
+    public float sprintSpeed = 9f;
+    public float moveSpeed = 7f;
     public float jumpHeight = 8f;
-
-    // private bool isMoving = false;
-    // private bool isSprinting =false;
+    private float playerSpeed;
     private bool rotateLeft = true;
     private bool rotateRight = false;
     private bool canJump = false;
     private bool canJumpWall = false;
+    private bool isAttacking = false;
     private Rigidbody2D rb;
+    private Animator animator;
 
-
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
+    void Start () {
+        rb = GetComponent<Rigidbody2D> ();
+        animator = GetComponent<Animator> ();
+        playerSpeed = moveSpeed;
     }
 
-
-    void Update()
-    {
-        moveCharacter();
+    void Update () {
+        moveCharacter ();
     }
-
-
-    void moveCharacter()
-    {
-        //Move right
-        if (Input.GetAxis("Horizontal") > 0)
-        {
-            if(rotateRight){
-                transform.Rotate(0,180,0);
-                rotateRight = false;
-                rotateLeft = true;
-            }
-            rb.velocity = new Vector2 (Input.GetAxis("Horizontal") * walkSpeed, rb.velocity.y);
-        }//Move left
-        else if(Input.GetAxis("Horizontal") < 0){
-            if(rotateLeft){
-                transform.Rotate(0,180,0);
-                rotateLeft = false;
-                rotateRight = true;
-            }
-            rb.velocity = new Vector2 (Input.GetAxis("Horizontal") * walkSpeed, rb.velocity.y);
-        }
-        //Jump
-        if(Input.GetAxis("Vertical") > 0){
-            if(canJump){
-                canJump = false;
-                rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
-            } 
-        }
-    } 
-    void OnCollisionEnter2D (Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Ground")
-        {
+    void OnCollisionEnter2D (Collision2D collision) {
+        if (collision.gameObject.tag == "Ground") {
             canJump = true;
             canJumpWall = false;
         }
-        if(collision.gameObject.tag == "Wall" && !canJumpWall && Input.GetAxis("Vertical") > 0){
+        if (collision.gameObject.tag == "Wall" && !canJumpWall && Input.GetAxis ("Vertical") > 0) {
             canJumpWall = true;
-            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+            rb.velocity = new Vector2 (rb.velocity.x, jumpHeight);
         }
     }
-
-    void OnCollisionExit2D(Collision2D collision) {
-        if (collision.gameObject.tag == "Ground")
-        {
+    void OnCollisionExit2D (Collision2D collision) {
+        if (collision.gameObject.tag == "Ground") {
             canJump = false;
         }
     }
+    void moveCharacter () {
+        animator.SetBool ("move", false);//move animate
+        ////////////////////MOVE RIGHT\\\\\\\\\\\\\\\\\\\\\
+        if (Input.GetAxis ("Horizontal") > 0) {
+            animator.SetBool ("move", true);//move animate
+            if (rotateRight) {
+                transform.Rotate (0, 180, 0);
+                rotateRight = false;
+                rotateLeft = true;
+            }
+            rb.velocity = new Vector2 (Input.GetAxis ("Horizontal") * playerSpeed, rb.velocity.y);
+        } ////////////////////MOVE LEFT\\\\\\\\\\\\\\\\\\\\\
+        else if (Input.GetAxis ("Horizontal") < 0) {
+            animator.SetBool ("move", true); //move animate
+            if (rotateLeft) {
+                transform.Rotate (0, 180, 0);
+                rotateLeft = false;
+                rotateRight = true;
+            }
+            rb.velocity = new Vector2 (Input.GetAxis ("Horizontal") * playerSpeed, rb.velocity.y);
+        }
+        ////////////////////SPRINT\\\\\\\\\\\\\\\\\\\\\
+        if (Input.GetKey (KeyCode.LeftShift)){
+            playerSpeed = sprintSpeed;
+        }
+        else{
+            playerSpeed = moveSpeed;
+        }
+        ////////////////////ATACK\\\\\\\\\\\\\\\\\\\\\
+        if (Input.GetKeyDown (KeyCode.Space) && !canJumpWall){
+            atack();
+        }
+        else{
+            animator.SetBool("atack1", false);
+            animator.SetBool("atack2", false);
+            animator.SetBool("atack3", false);
+        }
+        ////////////////////Jump\\\\\\\\\\\\\\\\\\\\\
+        if (Input.GetAxis ("Vertical") > 0) {
+            if (canJump) {
+                canJump = false;
+                rb.velocity = new Vector2 (rb.velocity.x, jumpHeight);
+            }
+        }
+    }
 
+    void atack(){
+        animator.SetBool("atack"+Random.Range(1,4), true);
+    }
 }
