b7b72ce23e4d5ff06e4e0361681eb6d9e449ee02
ArturoS159 <akowalski3677@gmail.com>
2019-11-28 23:41:29 +0100

    add coin, hearth

---
+++ Assets/Scripts/GameplayManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameplayManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    public void Die()
+    {
+        SceneManager.LoadScene("GameOver");
+
+    }
+    public void Restart()
+    {
+
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Heart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart : MonoBehaviour
+{
+    private float speed = 220.0f;
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D ot)
+    {
+        if (ot.gameObject.CompareTag("Player"))
+        {
+            GameplayController.instance.IncrementLife();
+        }
+    }
+}
