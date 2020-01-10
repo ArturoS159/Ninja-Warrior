2647ab623e7340f0091a0f076167ee1f7fc6c0d6
ArturoS159 <akowalski3677@gmail.com>
2020-01-10 14:43:46 +0100

    add bullet system

--- Assets/Scripts/TurretAttack.cs
+++ Assets/Scripts/TurretAttack.cs
@@ -1,18 +1,28 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TurretAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject target;
+    public GameObject startPos;
+    public GameObject bulletPrefab;
+    public float speedBullet = 7f;
+    private Vector2 moveDir;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    internal void attack(Collider2D colision){
+        if (colision.tag.CompareTo("Player").Equals(0))
+        {
+            GameObject bullet;
+            bullet = Instantiate(bulletPrefab, startPos.transform.position, startPos.transform.rotation);
+            //moveDir = (target.transform.position - transform.position).normalized * speedBullet;
+            //Debug.Log(moveDir);
+            //bullet.GetComponent<Rigidbody2D>().AddForce(target.transform.right * speedBullet * Time.deltaTime);
+
+            Vector2 dir = (target.transform.position).normalized;
+            Debug.Log(dir);
+            //bullet.GetComponent<Rigidbody2D>().AddForce(dir * 5 * Time.deltaTime, ForceMode2D.Force);
+        }
     }
 }
---
+++ Assets/Scripts/TurretRange.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretRange : MonoBehaviour
+{
+    private bool wait;
+    private void Start()
+    {
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+
+        if (!wait)
+        {
+            StartCoroutine(Wait(collision));
+        }
+
+    }
+    IEnumerator Wait(Collider2D collision)
+    {
+        wait = true;
+        yield return new WaitForSeconds(4);
+        GameObject.Find("tower").GetComponent<TurretAttack>().attack(collision);
+        wait = false;
+    }
+
+}
